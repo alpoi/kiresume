a81e4dc8330f4977099385b37faf48658dded104
alpoi <me@angus.buzz>
2025-05-08 00:26:40 +0100

    fix: ensure we have the font in our action, change release type

--- scripts/compile-examples.sh
+++ scripts/compile-examples.sh
@@ -1,3 +1,11 @@
+mkdir -p fonts
+if [ ! -f "./fonts/NotoSans-Regular.ttf" ]; then
+    curl -sL https://github.com/googlefonts/noto-fonts/raw/main/hinted/ttf/NotoSans/NotoSans-Regular.ttf -o ./fonts/NotoSans-Regular.ttf
+    curl -sL https://github.com/googlefonts/noto-fonts/raw/main/hinted/ttf/NotoSans/NotoSans-Bold.ttf -o ./fonts/NotoSans-Bold.ttf
+    curl -sL https://github.com/googlefonts/noto-fonts/raw/main/hinted/ttf/NotoSans/NotoSans-Italic.ttf -o ./fonts/NotoSans-Italic.ttf
+    curl -sL https://github.com/googlefonts/noto-fonts/raw/main/hinted/ttf/NotoSans/NotoSans-BoldItalic.ttf -o ./fonts/NotoSans-BoldItalic.ttf
+fi
+
 typst compile --font-path ./ --root ./ examples/example.typ examples/example.pdf
 typst compile --font-path ./ --root ./ examples/example.typ examples/example.png
 typst compile --font-path ./ --root ./ examples/example.typ examples/example.svg
